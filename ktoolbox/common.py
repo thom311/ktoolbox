thom311/ktoolbox

commit d114e7120d65a0f324ecf559273168d94990750b

common: add @iter_{listify,tuplify,dictify} decorators

ktoolbox/common.py
@@ -233,6 +233,34 @@ def iter_filter_none(lst: Iterable[Optional[T]]) -> Iterable[T]:
             yield v
 
 
+P = typing.ParamSpec("P")
+
+
+def iter_listify(fcn: typing.Callable[P, Iterable[T]]) -> typing.Callable[P, list[T]]:
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> list[T]:
+        return list(fcn(*args, **kwargs))
+
+    return wrapper
+
+
+def iter_tuplify(
+    fcn: typing.Callable[P, Iterable[T]]
+) -> typing.Callable[P, tuple[T, ...]]:
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> tuple[T, ...]:
+        return tuple(fcn(*args, **kwargs))
+
+    return wrapper
+
+
+def iter_dictify(
+    fcn: typing.Callable[P, Iterable[tuple[T1, T2]]]
+) -> typing.Callable[P, dict[T1, T2]]:
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> dict[T1, T2]:
+        return dict(fcn(*args, **kwargs))
+
+    return wrapper
+
+
 def unwrap(val: Optional[T], *, or_else: Optional[T] = None) -> T:
     # Like Rust's unwrap. Get the value or die (with an exception).
     #

tests/test_common.py
@@ -7,6 +7,7 @@ import random
 import sys
 import typing
 
+from collections.abc import Iterable
 from enum import Enum
 from typing import Any
 from typing import Optional
@@ -1236,3 +1237,82 @@ def test_path_norm() -> None:
         else:
             assert r2 == conf.normpath
             assert conf.result != conf.normpath
+
+
+def test_iter_listify() -> None:
+    @common.iter_listify
+    def f_lst_1() -> Iterable[int]:
+        yield 1
+        yield 2
+
+    if sys.version_info >= (3, 10):
+        typing.assert_type(f_lst_1(), list[int])
+
+    x_lst_1: list[int] = f_lst_1()
+    assert x_lst_1 == [1, 2]
+
+    @common.iter_listify
+    def f_lst_2() -> tuple[str, ...]:
+        return ("a", "b")
+
+    if sys.version_info >= (3, 10):
+        typing.assert_type(f_lst_2(), list[str])
+
+    x_lst_2: list[str] = f_lst_2()
+    assert x_lst_2 == ["a", "b"]
+
+    @common.iter_listify
+    def f_lst_3(*args: tuple[str, int]) -> dict[str, int]:
+        return dict(args)
+
+    if sys.version_info >= (3, 10):
+        typing.assert_type(f_lst_3(), list[str])
+
+    x_lst_3: list[str] = f_lst_3()
+    assert x_lst_3 == []
+
+    x_lst_3 = f_lst_3(("a", 1), ("b", 2))
+    assert x_lst_3 == ["a", "b"]
+
+    @common.iter_tuplify
+    def f_tpl_1() -> Iterable[str]:
+        yield "1"
+
+    if sys.version_info >= (3, 10):
+        typing.assert_type(f_tpl_1(), tuple[str, ...])
+
+    x_tpl_1: tuple[str, ...] = f_tpl_1()
+    assert x_tpl_1 == ("1",)
+
+    @common.iter_tuplify
+    @common.iter_tuplify
+    def f_tpl_2(x: float) -> list[float]:
+        return [1.4, 2.0, x]
+
+    if sys.version_info >= (3, 10):
+        typing.assert_type(f_tpl_2(5.0), tuple[float, ...])
+
+    x_tpl_2: tuple[float, ...] = f_tpl_2(5.0)
+    assert x_tpl_2 == (1.4, 2.0, 5.0)
+
+    @common.iter_dictify
+    def f_dict_1(a: int, b: str, c: str) -> Iterable[tuple[int, str]]:
+        return {a: b + c}.items()
+
+    if sys.version_info >= (3, 10):
+        typing.assert_type(f_dict_1(2, "", "x"), dict[int, str])
+
+    x_dict_1: dict[int, str] = f_dict_1(1, "b", "x")
+    assert x_dict_1 == {1: "bx"}
+
+    @common.iter_dictify
+    @common.iter_tuplify
+    def f_dict_2(a: int, b: str, c: str) -> Iterable[tuple[int, str]]:
+        yield a, b
+        yield 5, c
+
+    if sys.version_info >= (3, 10):
+        typing.assert_type(f_dict_2(2, "", "x"), dict[int, str])
+
+    x_dict_2: dict[int, str] = f_dict_2(1, "b", "x")
+    assert x_dict_2 == {1: "b", 5: "x"}

commit 47b22671dc5690d7307e38cf5b7815374c34a048

common: fix @iter_{listify,tuplify,dictify} for Python 3.9
typing.ParamSpec only exists in Python 3.10. For 3.9 support we need a
less powerful alternative that does not correctly detect the types of
the decorated function.

This is acceptable, because it works at runtime with Python 3.9. And
while having less strict type checks with 3.9, once you check with 3.10,
you get the strict checks.

Fixes: d114e7120d65 ('common: add @iter_{listify,tuplify,dictify} decorators')

ktoolbox/common.py
@@ -233,32 +233,70 @@ def iter_filter_none(lst: Iterable[Optional[T]]) -> Iterable[T]:
             yield v
 
 
-P = typing.ParamSpec("P")
+if sys.version_info >= (3, 10):
+    P = typing.ParamSpec("P")
+
+if sys.version_info >= (3, 10):
+
+    def iter_listify(
+        fcn: typing.Callable[P, Iterable[T]]
+    ) -> typing.Callable[P, list[T]]:
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> list[T]:
+            return list(fcn(*args, **kwargs))
+
+        return wrapper
+
+else:
+
+    def iter_listify(
+        fcn: typing.Callable[..., Iterable[T]]
+    ) -> typing.Callable[..., list[T]]:
+        def wrapper(*args: Any, **kwargs: Any) -> list[T]:
+            return list(fcn(*args, **kwargs))
+
+        return wrapper
 
 
-def iter_listify(fcn: typing.Callable[P, Iterable[T]]) -> typing.Callable[P, list[T]]:
-    def wrapper(*args: P.args, **kwargs: P.kwargs) -> list[T]:
-        return list(fcn(*args, **kwargs))
+if sys.version_info >= (3, 10):
 
-    return wrapper
+    def iter_tuplify(
+        fcn: typing.Callable[P, Iterable[T]]
+    ) -> typing.Callable[P, tuple[T, ...]]:
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> tuple[T, ...]:
+            return tuple(fcn(*args, **kwargs))
+
+        return wrapper
+
+else:
+
+    def iter_tuplify(
+        fcn: typing.Callable[..., Iterable[T]]
+    ) -> typing.Callable[..., tuple[T, ...]]:
+        def wrapper(*args: Any, **kwargs: Any) -> tuple[T, ...]:
+            return tuple(fcn(*args, **kwargs))
+
+        return wrapper
 
 
-def iter_tuplify(
-    fcn: typing.Callable[P, Iterable[T]]
-) -> typing.Callable[P, tuple[T, ...]]:
-    def wrapper(*args: P.args, **kwargs: P.kwargs) -> tuple[T, ...]:
-        return tuple(fcn(*args, **kwargs))
+if sys.version_info >= (3, 10):
 
-    return wrapper
+    def iter_dictify(
+        fcn: typing.Callable[P, Iterable[tuple[T1, T2]]]
+    ) -> typing.Callable[P, dict[T1, T2]]:
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> dict[T1, T2]:
+            return dict(fcn(*args, **kwargs))
 
+        return wrapper
 
-def iter_dictify(
-    fcn: typing.Callable[P, Iterable[tuple[T1, T2]]]
-) -> typing.Callable[P, dict[T1, T2]]:
-    def wrapper(*args: P.args, **kwargs: P.kwargs) -> dict[T1, T2]:
-        return dict(fcn(*args, **kwargs))
+else:
 
-    return wrapper
+    def iter_dictify(
+        fcn: typing.Callable[..., Iterable[tuple[T1, T2]]]
+    ) -> typing.Callable[..., dict[T1, T2]]:
+        def wrapper(*args: Any, **kwargs: Any) -> dict[T1, T2]:
+            return dict(fcn(*args, **kwargs))
+
+        return wrapper
 
 
 def unwrap(val: Optional[T], *, or_else: Optional[T] = None) -> T:

tests/test_common.py
@@ -1316,3 +1316,6 @@ def test_iter_listify() -> None:
 
     x_dict_2: dict[int, str] = f_dict_2(1, "b", "x")
     assert x_dict_2 == {1: "b", 5: "x"}
+
+    f_dict_2_x: typing.Callable[[int, str, str], dict[int, str]] = f_dict_2
+    assert f_dict_2_x is f_dict_2