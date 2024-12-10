91721752e91a58e7759e1123185d933c39a7ca79
Thomas Haller <thaller@redhat.com>
2024-12-10 16:56:42 +0100

    kjinka2: rename module "jinja2util" to "kjinja2"
    That is consistent with "kyaml" package. Also, it is a bit shorter
    and seems the slightly better name.

--- tests/test_kjinja2.py
+++ tests/test_kjinja2.py
@@ -2,12 +2,12 @@ import io
 
 from typing import Any
 
-from ktoolbox import jinja2util
+from ktoolbox import kjinja2
 
 
 def test_render() -> None:
     def _r(contents: str, **kwargs: Any) -> str:
-        return jinja2util.render_data(contents, kwargs)
+        return kjinja2.render_data(contents, kwargs)
 
     assert _r("", a="1") == ""
     assert _r("val: {{a}}", a=1) == "val: 1"
@@ -23,5 +23,5 @@ def test_render() -> None:
 def test_render_file() -> None:
     in_buffer = io.StringIO("val: {{a}}")
     out_buffer = io.StringIO()
-    jinja2util.render_file(in_buffer, out_buffer, {"a": 1})
+    kjinja2.render_file(in_buffer, out_buffer, {"a": 1})
     assert out_buffer.getvalue() == "val: 1"
